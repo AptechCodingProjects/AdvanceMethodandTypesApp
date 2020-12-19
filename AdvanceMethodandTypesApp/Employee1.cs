f5eec1726bd49f0517a5d449cc20f08d94c2706d
Muhammad Fahad <siddiquiboy360@outlook.com>
2020-12-19 18:36:56 +0500

    Add project files.

---
+++ AdvanceMethodandTypesApp/Employee1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvanceMethodandTypesApp
+{
+    partial class Employee
+    {
+        public void testmethod2()
+        {
+            Console.WriteLine("This file is from employee1.cs");
+        }
+    }
+}
